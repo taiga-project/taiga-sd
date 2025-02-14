410b1832a9b89047e4415265870077d0c79a7b7b
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2025-02-14 01:23:50 +0100

    Reorganise TAIGA-SD

--- include/core/init/generate_coords.cuh
+++
@@ -1,7 +0,0 @@
-#ifndef GENERATE_COORDS_CUH
-#define GENERATE_COORDS_CUH
-
-__global__ void generate_coords(TaigaGlobals *globals, BeamProp beam, BeamProfile *prof, double mass);
-
-#endif //GENERATE_COORDS_CUH
-
--- include/core/solvers/solvers.cuh
+++
@@ -1,6 +0,0 @@
-#ifndef SOLVERS_CUH
-#define SOLVERS_CUH
-
-__device__ void (*get_acceleration_from_lorentz_force)(double *a, double *v, double *B, double *E, double eperm);
-
-#endif //SOLVERS_CUH
